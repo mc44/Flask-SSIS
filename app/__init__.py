e6bcd92213c7e89a5dab5055f325cc05fa630f36
Marc Fajardo <marcfajardo14@gmail.com>
2022-01-24 17:13:37 +0800

    env setup for cloudinary credentials

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_mysql_connector import MySQL
 from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
+from config import CLOUD_NAME, API_KEY, API_SECRET
 from flask_wtf.csrf import CSRFProtect
 import cloudinary
 import cloudinary.uploader
@@ -21,10 +22,11 @@ def create_app(test_config=None):
         MYSQL_HOST=DB_HOST,
         #BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
     )
-    cloudinary.config( 
-        cloud_name = "dsemk8clb", 
-        api_key = "287298112411797", 
-        api_secret = "9QQ0JU44f7vucKqhAPY2FQrnRRg" 
+    print(API_KEY)
+    cloudinary.config(
+        cloud_name=CLOUD_NAME,
+        api_key=API_KEY,
+        api_secret=API_SECRET
     )
     bootstrap.init_app(app)
     mysql.init_app(app)
